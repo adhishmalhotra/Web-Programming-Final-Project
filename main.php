9ba8d10f4566bd48346e490da8b6ff554ecc8fcb
aiden5221 <aidenjr521@hotmail.com>
2020-11-04 20:54:50 -0500

    Navbar added along with css

--- main.php
+++ main.php
@@ -1,8 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="styles.css">
+
 <title></title>
+
 </head>
+
 <body>
+    
+<div id="header">
+    <h1>Sports and Events Management</h1>
+</div>
+
+<div id="navBar">
+    <a href="#home">Home</a><
+    <a href="#viewEvents">View Events</a>
+    <a href="#schedule">Schedule an Event</a>
+    <a href="#info">Information</a>
+    <a href="#contact">Contact</a>
+</div>
+
+
 </body>
 </html>
