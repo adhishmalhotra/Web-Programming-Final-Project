fa4f86b5f8f6b688323eeac5c20ba6751a0f436a
adhishmalhotra <adhish.malhotra@ontariotechu.net>
2020-11-05 21:15:31 -0500

    new nav bar

--- main.php
+++ main.php
@@ -1,26 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="styles.css">
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-<title></title>
+<title>FINAL PROJECT GROUP 18</title>
 
 </head>
 
 <body>
     
-<div id="header">
-    <header>Sports and Events Management</header>
-</div>
-
-<div id="navBar">
-    <a href="#home">Home</a><
-    <a href="#viewEvents">View Events</a>
-    <a href="#schedule">Schedule an Event</a>
-    <a href="#info">Information</a>
-    <a href="#contact">Contact</a>
-</div>
-
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <a href="#" class="navbar-brand">CAMPUS WELLNESS CENTRE</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse justify-content-end" id="navbarNavDropdown">
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a href="#" class="nav-link active">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="#viewEvents" class="nav-link">View Events</a>
+            </li>
+            <li class="nav-item">
+                <a href="#schedule" class="nav-link">Schedule an Event</a>
+            </li>
+            <li class="nav-item">
+                <a href="#info" class="nav-link">Information</a>
+            </li>
+            <li class="nav-item">
+                <a href="#contact" class="nav-link" data-toggle="modal">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 
 <div id="leftContent">
     <h1>Header</h2>
