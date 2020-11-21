6bca4f8037b96b538d822ed8cda7dad9a69a8cc5
MalaikaS-24 <73505090+MalaikaS-24@users.noreply.github.com>
2020-11-20 20:19:13 -0500

    Add files via upload

---
+++ Log In Java.js
@@ -0,0 +1,16 @@
+const loginForm = document.getElementById("LoginForm");
+const loginButton = document.getElementById("LoginButton");
+const loginErrorMsg = document.getElementById("ErrorMsg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === " *link usernames here* " && password === " *link password here* ") {
+        // I need a statment to try and link this to an external form with all the login 
+        //usernames and respective passwords. Then we can hyperlink it back to home page. 
+    } else {
+        loginErrorMsg.style.opacity = 3;
+    }
+})
